c557d6857f2d9aebb694bdadf05f4abfd25b7f96
indra <indrasaputraidrus@gmail.com>
2024-04-22 11:58:59 +0800

    install shadcn-ui

--- src/app/(main)/board/page.tsx
+++ src/app/(main)/board/page.tsx
@@ -1,10 +1,12 @@
 import { SignOutButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button"
 
 export default function BoardPage() {
   return (
-    <div>
-      <SignOutButton />
-      board list
+    <div className="p-10">
+      <SignOutButton redirectUrl="/">
+        <Button>Sing Out</Button>
+      </SignOutButton>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignInButton } from "@clerk/nextjs"
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button"
 
 export default function HomePage() {
 
@@ -14,7 +15,9 @@ export default function HomePage() {
     <main className="min-h-dvh p-24">
       <h1>task manager</h1>
       <div>
-        <SignInButton fallbackRedirectUrl="/board" mode="modal" />
+        <SignInButton fallbackRedirectUrl="/board" mode="modal">
+          <Button>Sign in</Button>
+        </SignInButton>
       </div>
     </main>
   );
